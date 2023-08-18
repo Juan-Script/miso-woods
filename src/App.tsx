0034cb455a83bcebd2ca75aa29fb4682cf1cbd7f
Juarrior <juanreig197@gmail.com>
2023-08-18 19:58:29 +0200

    Rutas modificadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
+// React
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Components
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { AboutUs } from "./pages/AboutUs";
 import { Products } from "./pages/Products";
-import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Router basename="/">
-        
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="products/" element={<Products />} />
+          <Route path="aboutus/" element={<AboutUs />} />
+          <Route path="contact/" element={<Contact />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
